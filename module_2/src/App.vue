615ff493a32dc82c30e7cd631bc32a52310cf051
ilane <ilane.bouttemant@epitech.eu>
2022-10-26 17:10:05 +0200

    commit theme 2 piscine

---
+++ module_2/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CurrentWeather from './components/CurrentWeather.vue';
+
+
+</script>
+
+<template>
+
+  
+  <CurrentWeather town = "Lyon" weather="12"/>
+    
+  
+</template>
+
+<style>
+
+</style>
---
+++ module_2/src/components/CurrentWeather.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import moment from 'moment/min/moment-with-locales'
+moment.locale('fr')
+defineProps({
+    town: {
+        type: String,
+        required :true
+    },
+    weather: {
+        type: String,
+        requiered: true
+    },
+    
+})
+
+</script>
+
+<template >
+        <div class="container">
+        <h1>météo à {{town}}</h1>
+        <h2>temperature {{weather}}</h2>
+        <h2>date : {{moment().format('LLLL')}} </h2>
+        <select>
+            
+        </select>
+        </div>
+
+        
+
+</template>
+<style>
+</style>
